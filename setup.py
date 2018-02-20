723eff3887117120c5ae8df2b9152f5c823a4e5f
Lars Immisch <lars@ibp.de>
2018-02-20 12:18:44 +0100

    Prepare next release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 from sys import version
 
-pyalsa_version = '0.8.4'
+pyalsa_version = '0.8.5'
 
 if __name__ == '__main__':
     setup(
