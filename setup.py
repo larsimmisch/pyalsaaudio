de2fc3c992efe6eb4c4a7eb1be16f472c77f0fdf
Oswald Buddenhagen <oswald.buddenhagen@gmx.de>
2022-08-06 17:45:50 +0200

    bump (minor) version
    we're about to add new features.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 from sys import version
 
-pyalsa_version = '0.9.2'
+pyalsa_version = '0.10.0'
 
 if __name__ == '__main__':
     setup(
