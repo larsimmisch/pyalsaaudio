larsimmisch/pyalsaaudio

commit c2a6b6e58307034ad8febd3f7d296c9bd8727ca6

reshuffle XRUN recovery somewhat
perform it prior to invoking read()/write() if necessary, not right
after a failure event. this makes things more uniform and predictable.

we don't use snd_pcm_recover() any more, as we used it only for the
EPIPE case anyway, which boils down to snd_pcm_prepare() exactly.
handling ESTRPIPE as well might be desirable, but that's a separate
consideration.

alsaaudio.c
@@ -1311,6 +1311,7 @@ alsapcm_setperiodsize(alsapcm_t *self, PyObject *args)
 static PyObject *
 alsapcm_read(alsapcm_t *self, PyObject *args)
 {
+	snd_pcm_state_t state;
 	int res;
 	int size = self->framesize * self->periodsize;
 	int sizeout = 0;
@@ -1344,14 +1345,13 @@ alsapcm_read(alsapcm_t *self, PyObject *args)
 	buffer = PyBytes_AS_STRING(buffer_obj);
 #endif
 
-	Py_BEGIN_ALLOW_THREADS
-	res = snd_pcm_readi(self->handle, buffer, self->periodsize);
-	if (res == -EPIPE)
-	{
-		/* EPIPE means overrun */
-		snd_pcm_prepare(self->handle);
+	state = snd_pcm_state(self->handle);
+	if ((state != SND_PCM_STATE_XRUN && state != SND_PCM_STATE_SETUP) ||
+		(res = snd_pcm_prepare(self->handle)) >= 0) {
+		Py_BEGIN_ALLOW_THREADS
+		res = snd_pcm_readi(self->handle, buffer, self->periodsize);
+		Py_END_ALLOW_THREADS
 	}
-	Py_END_ALLOW_THREADS
 
 	if (res != -EPIPE)
 	{
@@ -1404,6 +1404,7 @@ alsapcm_read(alsapcm_t *self, PyObject *args)
 
 static PyObject *alsapcm_write(alsapcm_t *self, PyObject *args)
 {
+	snd_pcm_state_t state;
 	int res;
 	int datalen;
 	char *data;
@@ -1435,16 +1436,13 @@ static PyObject *alsapcm_write(alsapcm_t *self, PyObject *args)
 		return NULL;
 	}
 
-	Py_BEGIN_ALLOW_THREADS
-	res = snd_pcm_writei(self->handle, data, datalen/self->framesize);
-	if (res == -EPIPE)
-	{
-		/* EPIPE means underrun */
-		res = snd_pcm_recover(self->handle, res, 1);
-		if (res >= 0)
-			res = snd_pcm_writei(self->handle, data, datalen/self->framesize);
+	state = snd_pcm_state(self->handle);
+	if ((state != SND_PCM_STATE_XRUN && state != SND_PCM_STATE_SETUP) ||
+		(res = snd_pcm_prepare(self->handle)) >= 0) {
+		Py_BEGIN_ALLOW_THREADS
+		res = snd_pcm_writei(self->handle, data, datalen/self->framesize);
+		Py_END_ALLOW_THREADS
 	}
-	Py_END_ALLOW_THREADS
 
 	if (res == -EAGAIN) {
 		rc = PyLong_FromLong(0);
@@ -1499,15 +1497,6 @@ static PyObject *alsapcm_drop(alsapcm_t *self)
 
 	res = snd_pcm_drop(self->handle);
 
-	if (res < 0)
-	{
-		PyErr_Format(ALSAAudioError, "%s [%s]", snd_strerror(res),
-					 self->cardname);
-
-		return NULL;
-	}
-
-	res = snd_pcm_prepare(self->handle);
 	if (res < 0)
 	{
 		PyErr_Format(ALSAAudioError, "%s [%s]", snd_strerror(res),

commit 0a8cd50b1fad91e63cc252b694c1c42f1d6dd78b

unbreak read buffer overrun handling
my commit c2a6b6e broke it big time; we'd now just paper over overruns.
:}

the previous handling was fundamentally correct, needing only two
adjustments:
- to recover from drop()/drain(), we need to call snd_pcm_prepare() when
  the stream state is SND_PCM_STATE_SETUP. notably, we must not do this
  when the state is SND_PCM_STATE_XRUN.
- we should error-check the unlikely case that the recovery from an xrun
  fails.

that way we now have two snd_pcm_prepare() call sites in read(), which
looks a bit messy, but it's actually correct.

as a drive-by, simplify the return value check of snd_pcm_prepare() -
values higher than zero are impossible.

alsaaudio.c
@@ -1364,12 +1364,23 @@ alsapcm_read(alsapcm_t *self, PyObject *args)
 	buffer = PyBytes_AS_STRING(buffer_obj);
 #endif
 
+	// After drop() and drain(), we need to prepare the stream again.
+	// Note that fresh streams are already prepared by snd_pcm_hw_params().
 	state = snd_pcm_state(self->handle);
-	if ((state != SND_PCM_STATE_XRUN && state != SND_PCM_STATE_SETUP) ||
-		(res = snd_pcm_prepare(self->handle)) >= 0) {
+	if ((state != SND_PCM_STATE_SETUP) ||
+		!(res = snd_pcm_prepare(self->handle))) {
+
 		Py_BEGIN_ALLOW_THREADS
 		res = snd_pcm_readi(self->handle, buffer, self->periodsize);
 		Py_END_ALLOW_THREADS
+
+		if (res == -EPIPE) {
+			// This means buffer overrun, which we need to report.
+			// However, we recover the stream, so the next PCM.read() will work
+			// again. If recovery fails (very unlikely), report that instead.
+			if (!(res = snd_pcm_prepare(self->handle)))
+				res = -EPIPE;
+		}
 	}
 
 	if (res != -EPIPE)